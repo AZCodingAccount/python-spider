dc2cc21b9564b4242c95b48c0a20f8c1b4267a24
HanAlbert <han892577@163.com>
2023-10-05 22:13:48 +0800

    update pyquery库

--- 第二章-网页数据的提取/pyquery库的使用.py
+++ 第二章-网页数据的提取/pyquery库的使用.py
@@ -7,10 +7,10 @@ html = '''
 <div>
     <ul id="container">
          <li class="item-0">first item</li>
-         <li class="item-1"><a href="link2.html">second item</a></li>
-         <li class="item-0 active"><a href="link3.html"><span class="bold">third item</span></a></li>
-         <li class="item-1 active"><a href="link4.html" class="active">fourth item</a></li>
-         <li class="item-0"><a href="link5.html">fifth item</a></li>
+         <li class="item-1"><a href="link2.html" class="link">second item</a></li>
+         <li class="item-2 active"><a href="link3.html" class="link"><span class="bold">third item</span></a></li>
+         <li class="item-3 active"><a href="link4.html" class="active" class="link">fourth item</a></li>
+         <li class="item-4"><a href="link5.html">fifth item</a></li>
      </ul>
  </div>
 '''
@@ -35,19 +35,50 @@ doc3 = pq(filename='test.html')
 
 # TODO:查找节点
 # 子节点
-# 1：find方法，这个相当于选择器分开写了，但是这个不是遍历每一个，是直接一次拿到符合条件的字符串
+# 1：find方法，这个相当于选择器分开写了，但是这个不是遍历每一个，是直接一次拿到符合条件的【字符串】
 items = doc1('#container')
 lis1 = items.find('.active')
-print(lis1)
-# 2：children方法，children会忽略孙子等节点只关心子节点
+# print(lis1)
+# 2：children方法，children会忽略孙子等节点只关心子节点，传入选择器筛选子节点
 lis2 = items.children('.active')
-print(lis2)
+# print(lis2)
+
 
 # 父节点
 # 1：parent方法，这个方法只会获取直接父节点
 parent = doc1('#container li').parent()
-print(parent)   # 获取到ul节点
+# print(parent)         # 获取到ul节点
 # 2：parents方法，获取到祖先节点
 parents = doc1('#container li').parents()
-print(parents)
+# print(parents)        # 获取到div和ul
+# 3：进一步筛选祖先节点
+parents_checked = doc1('#container li').parents('div')
+# print(parents_checked)  # 获取到div
 
+# 兄弟节点
+# 1：siblings方法
+single_li = doc1('#container .item-0')
+siblings_li = single_li.siblings()
+# print(siblings_li)        # 获取到所有li
+# 使用选择器过滤兄弟节点
+second_li = single_li.siblings('.item-1')
+# print(second_li)          # 获取到第二个li
+
+# 遍历节点
+items_li = doc1('#container li')
+# for li in items_li.items():
+# print(li.text())  # 打印所有li标签里面的文本
+
+# TODO:提取信息
+# 获取属性
+# 需求：1：获取第三个li的所有class属性，2：获取第三个li里面的a的href
+# 选中这个节点->调用attr方法得到属性值
+classes = doc1('#container .item-2').attr('class')
+a_href_value = doc1('#container .item-2 a').attr('href')
+# print('第三个li的所有属性值为：' + classes + "第三个li的a里面的href属性值为" + str(a_href_value))
+
+# 获取所有class="link"的a节点的href属性（！！！如果使用attr是不能实现的，只会返回节点中第一个的href，这个时候需要借助items这个方法遍历）
+a_s = doc1('.link')
+print(a_s)
+for a in a_s.items():
+    print(a.attr('href'))
