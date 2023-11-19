a3d5828aa0a27aadbc64d9fc65c0cae60b670ecb
HanAlbert <han892577@163.com>
2023-11-19 14:47:09 +0800

    网易有道翻译—python完整版

--- 第7章-JS逆向/网易有道翻译/发送请求.py
+++ 第7章-JS逆向/网易有道翻译/发送请求.py
@@ -1,6 +1,10 @@
+import base64
+
 import requests
 import time
 from hashlib import md5
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
 
 """
         逆向请求参数的思路是首先抓包，看到请求体里面有sign，时间戳，显然是需要逆向的。这时搜一下webtranslate（或者搜mysticTime等），然后找到加密入口
@@ -55,3 +59,30 @@ data = {
 # 发送请求
 res = requests.post(url, headers=headers, data=data)
 print(res.text)
+
+key = 'ydsecret://query/key/B*RGygVywfNBwpmBaZg*WT7SIOUP2T0C9WHMZN39j^DAdaZhAnxvGcCY6VYFwnHl'
+key=md5(key.encode('utf-8')).digest()
+iv = 'ydsecret://query/iv/C@lZe2YzHtZ2CYgaXKSVfsb7Y4QWHjITPPZ0nQp87fBeJ!Iv6v^6fvi2WN@bYpJ4'
+iv=md5(iv.encode('utf-8')).digest()
+data = res.text.replace(
+    "_", "/").replace("-", "+")
+"""
+t为加密的数据，o为key，n为iv，接下来就是阅读代码，alloc函数可能是在分配什么东西，不影响o=a的值，i.a.create...这个函数可能是标准的或者非标准的，因此
+先试一下能不能复现，不能的话再具体去扣逻辑
+            R = (t,o,n)=>{
+                if (!t)
+                    return null;
+                const a = e.alloc(16, y(o))
+                  , c = e.alloc(16, y(n))
+                  , r = i.a.createDecipheriv("aes-128-cbc", a, c);
+                let s = r.update(t, "base64", "utf-8");
+                return s += r.final("utf-8"),
+                s
+            }
+"""
+
+aes = AES.new(key=key, IV=iv, mode=AES.MODE_CBC)
+
+result= unpad(aes.decrypt(base64.b64decode(data)), 16).decode("utf-8")
+
+print(result)
